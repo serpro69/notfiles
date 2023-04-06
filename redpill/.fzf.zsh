cccbd13f6f6aaded32039f8465ed9f178fbdfdc3
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2023-04-06 18:55:12 +0200

    Add redpill configs

---
+++ redpill/.fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/sergio/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/sergio/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/sergio/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/sergio/.fzf/shell/key-bindings.zsh"
