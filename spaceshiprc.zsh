ff8edaf88b3dadd2e78cde7733a9a81190ca7444
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-09-05 19:38:17 +0200

    Set spaceship sections to use async
    Modified async.zsh in spaceship submodule as suggested in
    https://github.com/spaceship-prompt/spaceship-prompt/issues/1193,
    which seems to have fixed issues with sections not showing up.
    
    Have to either disable async for everything, or use this hack until a
    better fix is pushed upstream.

--- spaceshiprc.zsh
+++ spaceshiprc.zsh
@@ -1,11 +1,11 @@
 # Git Options - https://spaceship-prompt.sh/sections/git/
 # Render git synchronously (https://spaceship-prompt.sh/config/prompt/#Asynchronous-rendering)
 SPACESHIP_GIT_ASYNC=false
-SPACESHIP_TERRAFORM_ASYNC=false
+SPACESHIP_TERRAFORM_ASYNC=true
 SPACESHIP_TERRAFORM_SYMBOL="󱁢 "
-SPACESHIP_PULUMI_ASYNC=false
+SPACESHIP_PULUMI_ASYNC=true
 # custom section
-SPACESHIP_TFB_ASYNC=false
+SPACESHIP_TFB_ASYNC=true
 SPACESHIP_TFB_PREFIX=""
 SPACESHIP_TFB_SUFFIX=" "
 SPACESHIP_TFB_SYMBOL="󱐕 "
